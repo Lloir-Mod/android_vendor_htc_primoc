11e3d5886f8d79c3cf79ba3e2fc42373d10ece4b
jmztaylor <jmztaylor@gmail.com>
2012-08-12 14:36:47 -0300

    Update primoc-vendor-blobs.mk

--- primoc-vendor-blobs.mk
+++ primoc-vendor-blobs.mk
@@ -53,7 +53,6 @@ PRODUCT_COPY_FILES += \
 # QCOM Drivers
 PRODUCT_COPY_FILES += \
     vendor/htc/primoc/proprietary/lib/libC2D2.so:/system/lib/libC2D2.so \
-    vendor/htc/primoc/proprietary/lib/libc2d2_z180.so:system/lib/libc2d2_z180.so \
     vendor/htc/primoc/proprietary/lib/libgsl.so:/system/lib/libgsl.so \
     vendor/htc/primoc/proprietary/lib/libOpenVG.so:/system/lib/libOpenVG.so \
     vendor/htc/primoc/proprietary/lib/libsc-a2xx.so:/system/lib/libsc-a2xx.so \
@@ -61,7 +60,6 @@ PRODUCT_COPY_FILES += \
     vendor/htc/primoc/proprietary/lib/egl/libEGL_adreno200.so:/system/lib/egl/libEGL_adreno200.so \
     vendor/htc/primoc/proprietary/lib/egl/libGLESv1_CM_adreno200.so:/system/lib/egl/libGLESv1_CM_adreno200.so \
     vendor/htc/primoc/proprietary/lib/egl/libGLESv2_adreno200.so:/system/lib/egl/libGLESv2_adreno200.so \
-    vendor/htc/primoc/proprietary/lib/egl/libGLESv2S3D_adreno200.so:/system/lib/egl/libGLESv2S3D.so \
     vendor/htc/primoc/proprietary/lib/egl/libq3dtools_adreno200.so:/system/lib/egl/libq3dtools_adreno200.so
     
 # Camera
